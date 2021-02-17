e14143db41fa2343159f47fd9e1068e284bb9185
Doritanh <Doritanh@Doritanh.home>
2021-02-17 18:13:30 +0100

    Better code

--- src/main/java/fr/doritanh/olurwa/core/listeners/MessageListener.java
+++ src/main/java/fr/doritanh/olurwa/core/listeners/MessageListener.java
@@ -23,18 +23,17 @@ public class MessageListener implements Listener {
 	
 	@EventHandler
 	public void onChatEvent(ChatEvent e) {
-		if (!e.isCommand() && !e.isProxyCommand() && e.getSender() instanceof ProxiedPlayer) {
-			e.setCancelled(true);
-			ProxiedPlayer player = (ProxiedPlayer)e.getSender();
-			CachedMetaData user = Core.getInstance().getLuckPerms().getPlayerAdapter(ProxiedPlayer.class).getMetaData(player);
-			String message = player.getName() + ": " + e.getMessage();
-			if (user.getPrefix() != null) {
-				message = user.getPrefix() + ChatColor.RESET + player.getName() + ": " + e.getMessage();
-			}
-			message = ChatColor.translateAlternateColorCodes('&', message);
-			for (ProxiedPlayer other : Core.getInstance().getProxy().getPlayers()) {
-				other.sendMessage(new TextComponent(message));
-			}
+		if (e.isCommand() || e.isProxyCommand() || !(e.getSender() instanceof ProxiedPlayer)) return;
+		e.setCancelled(true);
+		ProxiedPlayer player = (ProxiedPlayer)e.getSender();
+		CachedMetaData user = Core.getInstance().getLuckPerms().getPlayerAdapter(ProxiedPlayer.class).getMetaData(player);
+		String message = player.getName() + ": " + e.getMessage();
+		if (user.getPrefix() != null) {
+			message = user.getPrefix() + ChatColor.RESET + player.getName() + ": " + e.getMessage();
+		}
+		message = ChatColor.translateAlternateColorCodes('&', message);
+		for (ProxiedPlayer other : Core.getInstance().getProxy().getPlayers()) {
+			other.sendMessage(new TextComponent(message));
 		}
 	}
 	
