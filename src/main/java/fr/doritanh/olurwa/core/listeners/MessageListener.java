3ce9a3a6d0d74836063669e35a5a030f0274ad33
BuildTools <unconfigured@null.spigotmc.org>
2021-02-27 18:55:12 +0100

    Adjust core

--- src/main/java/fr/doritanh/olurwa/core/listeners/MessageListener.java
+++ src/main/java/fr/doritanh/olurwa/core/listeners/MessageListener.java
@@ -6,6 +6,7 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ChatEvent;
+import net.md_5.bungee.api.event.TabCompleteResponseEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
@@ -25,6 +26,11 @@ public class MessageListener implements Listener {
 		for (ProxiedPlayer other : Core.getInstance().getProxy().getPlayers()) {
 			other.sendMessage(new TextComponent(message));
 		}
+	}
+	
+	@EventHandler
+	public void onTabCompleteResponse(TabCompleteResponseEvent e) {
+		e.setCancelled(true);
 	}
 	
 }
